4e64f08b9870aa71b35d34d34259f8b6cb0fcdf1
Alejandro Escareno <alejandroef04@gmail.com>
2022-06-28 20:02:43 -0500

    Haciendo menu hamburguesa para mobile

--- src/js/carrousel.js
+++ src/js/carrousel.js
@@ -36,7 +36,6 @@ function initArrows() {
     const iLeft = document.createElement('i');
     iLeft.classList.add('fa')
     iLeft.classList.add('fa-arrow-left')
-    iLeft.innerHTML = '<svg class="arrow" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>'
     leftArrow.classList.add('slider-left')
     leftArrow.appendChild(iLeft)
     leftArrow.addEventListener('click', () => {
@@ -46,7 +45,6 @@ function initArrows() {
     const iRight = document.createElement('i');
     iRight.classList.add('fa')
     iRight.classList.add('fa-arrow-right')
-    iRight.innerHTML = '<svg class="arrow" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>'
     rightArrow.classList.add('slider-right')
     rightArrow.appendChild(iRight)
     rightArrow.addEventListener('click', () => {
